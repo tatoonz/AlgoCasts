94cf77206c456c178474a55c0e31cc2afd8c4fb7
TaToonz <peerapath.t@gmail.com>
2018-10-08 22:08:04 +0700

    feat(exercise): add solution for reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,17 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reversed = ''
+    for (let char of str) {
+        reversed = char + reversed;
+    }
+
+    return reversed;
+}
 
 module.exports = reverse;
