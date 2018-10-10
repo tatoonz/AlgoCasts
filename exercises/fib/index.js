c71079f1a0005d3b0ef7c8ff9cff3eaabcfbc309
TaToonz <peerapath.t@gmail.com>
2018-10-10 10:19:36 +0700

    feat(exercise): add solution for fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,25 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  if (n <= 0) {
+    return 0
+  }
+
+  if (n <= 2) {
+    return 1
+  }
+
+  let penultimate = 1
+  let last = 1
+
+  for (i = 3; i <= n; i++) {
+    const tmp = penultimate + last
+    penultimate = last
+    last = tmp
+  }
+
+  return last
+}
 
 module.exports = fib;
